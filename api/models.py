e281a26d1826b314ff930ea0e6365196e3ce576a
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-24 18:31:09 -0300

    making a simulation

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from decimal import Decimal
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'))
+    win_game = models.BooleanField()
+    email = models.EmailField(unique=True)
