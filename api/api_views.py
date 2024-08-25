5b83ca04932841e1d340f3f3b9be2d435e5b7c2e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-25 09:36:58 -0300

    resolving bugs rest

--- api/api_views.py
+++ api/api_views.py
@@ -1,10 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['POST'])
-def send_data(request):
-    return Response({'value': request.POST.get('value')})
-
 @api_view(['GET'])
 def create_user(request):
     return Response({'response': 'criando'})
--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from api import api_views
 
 urlpatterns = [
-    path('send/', api_views.send_data, name='send'),
     path('create/', api_views.create_user, name='create'),
     path('login/', api_views.login, name='login')
 ]
